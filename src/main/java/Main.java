2ca39c517dda99ad1ca1d5c3d4d466cbf886cb75
Manas Gandhi <manas.25bcs10617@sst.scaler.com>
2026-06-13 16:15:22 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,8 +18,40 @@ public class Main {
                 continue;
             }
 
+            /*
+             * if we use contains then if other command has echo then this will also execute
+             * which is incorrect
+             */
+
             if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
+                continue;
+            }
+
+            /*
+             * if i use startwith and endswith then type myecho will run and it will give
+             * output of myecho is a shell builtin
+             * 
+             * if(input.startsWith("type ")){
+             * if(input.endsWith("echo") || input.endsWith("type") ||
+             * input.endsWith("exit")){
+             * System.out.println(input.substring(5) + " is a shell builtin");
+             * } else {
+             * System.out.println(input.substring(5) + ": not found");
+             * }
+             * continue;
+             * }
+             */
+
+            if (input.startsWith("type ")) {
+                String target = input.substring(5);
+
+                if (target.equals("echo") || target.equals("exit") || target.equals("type")) {
+                    System.out.println(target + " is a shell builtin");
+                } else {
+                    System.out.println(target + ": not found");
+                }
+
                 continue;
             }
 
