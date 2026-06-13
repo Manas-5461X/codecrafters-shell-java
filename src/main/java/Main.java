b95d838d1d47a3a89379a6f263c57762b39c7681
Manas Gandhi <manas.25bcs10617@sst.scaler.com>
2026-06-13 15:24:39 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
-        System.out.print("$ ");
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("$ ");
+            System.out.flush();
+
+            String input = sc.nextLine();
+
+            if (input.isEmpty()) {
+                continue;
+            }
+
+            System.out.println(input + ": command not found");
+        }
 
     }
 }
